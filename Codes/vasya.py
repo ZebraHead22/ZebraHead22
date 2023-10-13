5a55f559e6124f895beebdbd9215e149f2a4e726
ZebraHead22 <baranovma1993@gmail.com>
2023-10-13 10:58:19 +0300

    new Vas

--- Codes/vasya.py
+++ Codes/vasya.py
@@ -1,14 +1,19 @@
 import os
 import time
 
-'''
-Переименовываем видео файлы, записывая дату создания
-в имя
-'''
-direct = os.getcwd()
-files = os.listdir(direct)
-for i in files:
-    filename, file_extension = os.path.splitext(i)
-    if file_extension == ".MOV":
-        new_name = time.ctime(os.path.getmtime(i))
-        os.rename(i, direct+"/"+str(new_name)+".MOV")
+def rename_video():
+    '''
+    Переименовываем видео файлы, записывая дату создания
+    в имя
+    '''
+    direct = os.getcwd()
+    files = os.listdir(direct)
+    for i in files:
+        filename, file_extension = os.path.splitext(i)
+        if file_extension == ".MOV":
+            new_name = time.ctime(os.path.getmtime(i))
+            os.rename(i, direct+"/"+str(new_name)+".MOV")
+
+
+def new_131023():
+    pass
